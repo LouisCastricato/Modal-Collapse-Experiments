Fixed graphs to compute variance over distances to centroid

main.py
@@ -1,6 +1,7 @@
 from utils import *
 import matplotlib.pyplot as plt
-
+from functools import partial
+from multiprocessing import Pool
 
 def generate_data(data_points=1000, dim=512, rotation_count=1, generate_function=get_hypersphere_points):
     # get hyper sphere points
@@ -31,7 +32,7 @@ def get_k_means_variance(data, k):
         return get_where_index(data, clusters, cluster_idx)
 
     # get the variance of each cluster
-    return [np.var(get_cluster(i)) for i in range(k)]
+    return [np.var(compute_distances_from_centroid(get_cluster(i))) for i in range(k)]
 
 
 if __name__ == '__main__':
@@ -43,8 +44,10 @@ if __name__ == '__main__':
     rotation_count = 1
 
     # generate data
-    hypersphere_data = generate_data(data_points, dim, rotation_count, generate_function=get_hypersphere_points)
-    splooch_data = generate_data(data_points, dim, rotation_count, generate_function=get_splooch_points)
+    hypersphere_data = generate_data(data_points, dim, rotation_count,
+        generate_function=get_hypersphere_points)
+    splooch_data = generate_data(data_points, dim, rotation_count,
+        generate_function=partial(get_splooch_points, scale_upper_bound=0.25, splooches=100))
 
     # interpolate between the two datasets
     def linear_interpolate(dataset1, dataset2):
@@ -58,13 +61,18 @@ if __name__ == '__main__':
 
     # interpolate
     interp = linear_interpolate(hypersphere_data, splooch_data)
-    for i in range(10):
-        # get variance
-        variance = get_k_means_variance(interp(float(i)/10.), cluster_count)
+    # multiprocessing
+    pool = Pool(processes=8)
+    # get variance
+    variances = pool.map(partial(get_k_means_variance, k=cluster_count), [interp(i/10) for i in range(10)])
+    pool.close()
 
+
+    
+    for idx, var in enumerate(variances):
         # compute a histogram using matplotlib
-        plt.hist(variance)
+        plt.hist(var)
         plt.show()
-        plt.savefig('graphs/variance_interp_' + str(i) + '.png')
+        plt.savefig('graphs/variance_interp_' + str(idx) + '.png')
         plt.clf()
 

utils.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, DBSCAN
 from scipy.stats import ortho_group
 import skdim.datasets
 
@@ -15,6 +15,17 @@ def compute_kmeans(data, k):
     """
     kmeans = KMeans(n_clusters=k).fit(data)
     return kmeans
+
+def compute_distances_from_centroid(data):
+    """
+    Computes the distances of each data point from the centroid of the data.
+    :param data: numpy array of shape (n, d)
+    :return: numpy array of shape (n, 1)
+    """
+    # compute centroid
+    centroid = np.mean(data, axis=0)
+    # compute distances
+    return np.linalg.norm(data - centroid, axis=1)
 
 def get_where_index(data, km, label_index):
     """
@@ -39,7 +50,7 @@ def get_normalized_hypercube_points(set_size=1000, dim=512):
     output = output / np.linalg.norm(output, axis=1).reshape(-1, 1)
     return output
 
-def get_splooch_points(set_size=1000, dim=512, splooches=10):
+def get_splooch_points(set_size=1000, dim=512, splooches=10, scale_upper_bound=1.0):
     """
     Generates a set of points that splooch the surface of a hyper sphere
     """
@@ -48,7 +59,7 @@ def get_splooch_points(set_size=1000, dim=512, splooches=10):
     for i in range(splooches):
         splooch = get_hypersphere_points(points_per_splooch, dim)
         random_direction = np.random.uniform(-1, 1, dim)
-        random_scale = np.random.uniform(0, 1)
+        random_scale = np.random.uniform(0, scale_upper_bound)
 
         splooch = splooch * random_scale + random_direction
 